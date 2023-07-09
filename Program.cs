9e2b64f92add3d972b004fb7cf3402c9c748e787
benjysugi <benjysugi01@outlook.com>
2023-07-10 00:55:29 +0100

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -25,31 +25,7 @@ namespace YT2013.UploadTool {
                 Application.Exit();
             }
 
-            using (var wc = new WebClient()) {
-                string latestVer = wc.DownloadString("https://yt2013.secretclub.cloud/Application/GetLatestRevision.ashx");
-
-                if(!latestVer.Contains("2.")) {
-                    if (latestVer == currentVersion) {
-                        Application.Run(new Form1());
-                    }
-                    else {
-                        if(!Directory.Exists(Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments) + "\\FakeTube")) {
-                            new Thread(() => {
-                                Thread.CurrentThread.IsBackground = true;
-
-                                MessageBox.Show("A new version is available and has been placed on the test branch and will launch shortly.\nPlease don't attempt to launch another instance", "FakeTube", MessageBoxButtons.OK, MessageBoxIcon.Information);
-                            }).Start();
-
-                            YT2013.UpdaterExtensions.Class1.Update();
-                        }
-
-                        Application.Run(new Form1());
-                    }
-                }
-                else {
-                    MessageBox.Show("This application's Windows Installer configuration needs to be updated and the updater doesn't have the necessary permissions to do so. Please redownload and reinstall the installer package from #upload-videos in order to use this tool.");
-                }
-            }
+            Application.Run(new Form1());
         }
     }
 }
